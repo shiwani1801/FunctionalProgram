fa30fe482c68df96e5204c6a824abaa2b2a554f9
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 06:21:25 -0800

    [shiwani]even and odd program is added

---
+++ src/com/bridgelabz/functionalprogram/EvenOdd.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.functionalprogram;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no.:");
+        n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("the no. "+n+" is even");
+        }else {
+            System.out.println("the no. "+n+" is odd");
+        }
+
+    }
+}
