1434c263dee4fbf5015a5fea12819371cde186e5
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 06:51:33 -0800

    [shiwani]QuotientRemainder  program is added

---
+++ src/com/bridgelabz/functionalprogram/QuotientRemainder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.functionalprogram;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+
+        int dividend = 150447, divisor = 61;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient="+quotient);
+        System.out.println("Remainder="+remainder);
+    }
+}
