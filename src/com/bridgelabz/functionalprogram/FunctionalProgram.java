0c51147f6501c8132e2b1d10d79372039923fe38
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 06:17:27 -0800

    [shiwani]functional and basic programs are added

---
+++ src/com/bridgelabz/functionalprogram/FunctionalProgram.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.functionalprogram;
+
+public class FunctionalProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the basic and functional program");
+    }
+}
