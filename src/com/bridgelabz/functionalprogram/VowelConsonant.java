50435ad9f4dd3c931fac67c41f6747e7ce0cd2ff
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 06:57:11 -0800

    [shiwani]vowel consonant  program is added

---
+++ src/com/bridgelabz/functionalprogram/VowelConsonant.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.functionalprogram;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        String c ="e";
+
+        switch(c) {
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            case"A":
+            case "E":
+            case "I":
+            case "O":
+            case "U":
+
+                System.out.println("vowel");
+                break;
+            default:
+                System.out.println("consonant");
+        }
+    }
+}
